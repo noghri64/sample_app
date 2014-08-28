952c230e0546fa8a3c8253d025771d57edfc9f92
Christian Beauchesne <cbeauchesne@gmail.com>
2014-08-28 10:11:42 -0400

    add a full title help

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,22 +2,40 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+ describe "Contact Page" do
+
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+  end
   describe "Home page" do
 
-    it "should have the right title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Home Page')
     end
+     it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+ 
   end
   describe "Help Page" do
     
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
     it "should have the content 'Help'" do
       visit '/static_pages/help'
@@ -28,7 +46,7 @@ describe "Static pages" do
     
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
